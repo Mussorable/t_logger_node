4cd2be63a7f0ecf25a43ff7c9d9a2b80aafcf63c
Mussorable <redduck5601@gmail.com>
2025-01-28 20:03:36 +0100

    added cron task to update and delete old legends

--- app/app.js
+++ app/app.js
@@ -6,13 +6,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { sequelize } = require('./models');
 const cron = require('node-cron');
-const { Note, Record } = require('./models');
+const { Note, Record, Legend } = require('./models');
 
 // ------
 
 const authRoutes = require('./routes/auth');
 const trucksRoutes = require('./routes/trucks');
 const notesRoutes = require('./routes/notes');
+const {Op} = require("sequelize");
 
 // ------
 
@@ -34,8 +35,23 @@ app.use('/notes', notesRoutes);
 cron.schedule("0 1 * * *", async () => {
     try {
         console.log('Starting daily cleanup...');
+
+        // Delete Daily Notes
         await Note.destroy({ where: {} });
+        // Delete Done truck notes
         await Record.destroy({ where: { type: "DONE" } });
+        // Mark Legend type DONE every route with date less than today
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        await Legend.update({ status: "DONE" }, { where: { date: { [Op.lt]: today } } });
+        // Delete Legends, where date less than today more than week
+        const oneWeekAgo = new Date(today);
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+        oneWeekAgo.setHours(0, 0, 0, 0);
+
+        await Legend.destroy({ where: { date: { [Op.lt]: oneWeekAgo } } });
+
         console.log('Cleanup finished successfully.');
     } catch(e) {
         console.error('Error while running cron', e);
